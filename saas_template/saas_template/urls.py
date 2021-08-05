8ea32213695205e98eb142d262bc18d123a882b4
slawomir <slawomir.and1@gmail.com>
2021-08-05 21:07:24 +0200

    no message

--- saas_template/saas_template/urls.py
+++ saas_template/saas_template/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from apps.core.views import frontpage
+from apps.core.views import frontpage, privacy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', frontpage, name='frontpage')
+    path('', frontpage, name='frontpage'),
+    path('privacy/', privacy, name='privacy'),
 ]
