2add5d4b8536cf7712394743310e409efbac7b78
slawomir <slawomir.and1@gmail.com>
2021-08-06 14:23:30 +0200

    no message

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/log-in',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/dashboard/Dashboard.vue'),
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: () => import('../views/dashboard/MyAccount.vue'),
+    meta: {
+      requireLogin: true
+    }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/log-in')
+  } else {
+    next()
+  }
 })
 
 export default router
