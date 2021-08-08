4c9793620ae0f155feaed978f65f8ba358e21243
slawomir <slawomir.and1@gmail.com>
2021-08-08 17:11:32 +0200

    no message

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
     'lead',
+    'team',
 ]
 
 MIDDLEWARE = [
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,15 +4,23 @@ export default createStore({
   state: {
     isLoading: false,
     isAuthenticated: false,
-    token: ''
+    token: '',
+    user: {
+      id: null,
+      username: ''
+    }
   },
   mutations: {
     initializeStore(state) {
       if (localStorage.getItem('token')) {
         state.token = localStorage.getItem('token')
+        state.user.id = localStorage.getItem('userid')
+        state.user.username = localStorage.getItem('username')
         state.isAuthenticated = true
       } else {
         state.token = ''
+        state.user.id = null
+        state.user.username = ''
         state.isAuthenticated = false
       }
     },
